60a4c264c0967b00d15e5d70121a2e95c8d048f0
Elwardi <elwardi.fadeli@tu-darmstadt.de>
2025-07-08 16:10:42 +0200

    test: support for parallel runs

--- tests/icsFoamTests.C
+++ tests/icsFoamTests.C
@@ -7,53 +7,31 @@ using namespace Foam;
 extern Time *timePtr;
 extern argList *argsPtr;
 
-TEST_CASE("Implicitly coupled source term gets resolved",
+const int N_PARALLEL_RUNS = 10;
+
+TEST_CASE("Native implementation of coupled equations",
           "[theCase][serial][parallel]") {
-  FatalError.dontThrowExceptions();
-  Time &runTime = *timePtr;
-  argList &args = *argsPtr;
-#include "createMesh.H"
-  volScalarField T(IOobject("T", runTime.timeName(), mesh, IOobject::MUST_READ,
-                            IOobject::NO_WRITE),
-                   mesh);
-  volScalarField Ts(IOobject("Ts", runTime.timeName(), mesh,
-                             IOobject::MUST_READ, IOobject::NO_WRITE),
-                    mesh);
-  forAll(T.internalField(), ci) {
-    T[ci] = 300 * Foam::sin(2 * mesh.C()[ci].x()) * Foam::sin(mesh.C()[ci].y());
-    T[ci] = 200 * Foam::sin(mesh.C()[ci].x()) * Foam::sin(2 * mesh.C()[ci].y());
+  #include "testSetup.H"
+  for(int i=0; i<N_PARALLEL_RUNS; i++) {
+    volatile bool out = implicitSourceCoupling::solveCoupledEqns(mesh, config);
+    CHECK(out);
   }
-  volVectorField U(IOobject("U", runTime.timeName(), mesh, IOobject::MUST_READ,
-                            IOobject::NO_WRITE),
-                   mesh);
-  dictionary config;
-  dimensionedScalar DT("DT", dimLength * dimVelocity, 1.0);
-  config.set("DT", DT);
-  dimensionedScalar DTs("DTs", dimLength * dimVelocity, 2.0);
-  config.set("DTs", DTs);
-  dimensionedScalar alpha("alpha", dimless / dimTime, 10.0);
-  config.set("alpha", alpha);
-
-#include "createPhi.H"
-
-  BENCHMARK("Native implementations of coupled equations") {
-    return implicitSourceCoupling::solveCoupledEqns(mesh, config);
-  };
-
-
-#if defined(OPENFOAM)
-  if (!Pstream::parRun()) {
-    MPI_Init(NULL, NULL);
-  }
-
-  BENCHMARK("PetSc implementation of coupled equations") {
-    return implicitSourceCoupling::solvePetScEqns(mesh, config);
-  };
-
-  if (!Pstream::parRun()) {
-    MPI_Finalize();
-  }
-#endif
-
+  Info().stdStream().clear();
   runTime.setTime(0.0, 0);
 }
+
+#if defined(OPENFOAM)
+
+TEST_CASE("PetSc implementation of coupled equations",
+          "[theCase][serial][parallel]") {
+  #include "testSetup.H"
+  if (!Pstream::parRun()) MPI_Init(NULL, NULL);
+  for(int i=0; i<N_PARALLEL_RUNS; i++) {
+    volatile bool out = implicitSourceCoupling::solvePetScEqns(mesh, config);
+    CHECK(out);
+  }
+  if (!Pstream::parRun()) MPI_Finalize();
+  Info().stdStream().clear();
+  runTime.setTime(0.0, 0);
+}
+#endif
